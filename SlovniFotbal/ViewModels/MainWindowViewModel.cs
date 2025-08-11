2501c81420f961f759ec095f1fafe4111c0a3278
Filip Vondrák <filip.vondrak4@seznam.cz>
2025-08-11 15:12:42 +0200

    Initial

---
+++ SlovniFotbal/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace SlovniFotbal.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ SlovniFotbal/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SlovniFotbal.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
